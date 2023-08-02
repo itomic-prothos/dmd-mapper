c4fc7cb3b18cae940198ac45829e056d8eb169c8
Ivan Silic Tomic <ivan.silic-tomic@rbinternational.com>
2023-08-03 01:09:20 +0200

    initial spring service setup

---
+++ src/main/java/eu/prothos/dmd/dmdmapper/DmdMapperApplication.java
@@ -0,0 +1,13 @@
+package eu.prothos.dmd.dmdmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmdMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmdMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/prothos/dmd/dmdmapper/DmdMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.prothos.dmd.dmdmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmdMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
